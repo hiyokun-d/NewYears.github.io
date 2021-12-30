38e91244d44c369a20b16b881cd10ab5f2810e24
hiyo <87683897+hiyokun-d@users.noreply.github.com>
2021-12-31 00:01:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -219,7 +219,7 @@ function style() {
         distance = countDownDate - now;
 
         // Time calculations for days, hours, minutes and seconds
-        days = Math.floor(distance / (1000 * 60 * 60 * 24)) - 1;
+        days = Math.floor(distance / (1000 * 60 * 60 * 24));
         hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         seconds = Math.floor((distance % (1000 * 60)) / 1000);
